0378090461e10a96ed2430ead43961b9143117ec
gseive64 <geoffroyseive@gmail.com>
2014-08-27 14:28:46 -0500

    Replaced template code with File Generation code

--- PythonCode/PythonCode.py
+++ PythonCode/PythonCode.py
@@ -1,2 +1,15 @@
-print('Hello World')
-print('Test')
+import random
+import time
+import datetime
+print('Automatic Generation')
+i = 0
+while True:
+    i += 1
+    f = open('C:\\Users\\gseive\\Documents\\tests\\test' + str(i) + '.txt', 'w')
+    f.write('Rank of file squared: ' + str(i ** 2) + ' | Random value between 1 and 5: ' + str(random.randint(0,5)) + '\n')
+    t = time.strftime("%H:%M:%S")
+    f.write("Current time is: " + str(t))
+    f.close()
+    if i % 10 == 0:
+        print(str(i) + ' files generated.')
+    time.sleep(random.randint(0,10))
